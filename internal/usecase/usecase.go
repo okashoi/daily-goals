7a4651f00559ad9d55222c08dd2bf9074d3f3584
Okada Shohei <okashoicircus0@gmail.com>
2024-02-24 00:11:07 +0900

    add validation

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"errors"
 	"github.com/okashoi/daily-goals/pkg/daily_goals"
 	"time"
 )
@@ -11,6 +12,14 @@ type Input struct {
 }
 
 func DoUsecase(input Input, config daily_goals.Config) error {
+	if input.SleepScore < 0 || input.SleepScore > 100 {
+		return errors.New("sleep score must be between 0 and 100")
+	}
+
+	if input.Date.After(time.Now()) {
+		return errors.New("date must be before tomorrow")
+	}
+
 	data := daily_goals.PostPixelData{
 		Date:     input.Date,
 		Quantity: input.SleepScore,
